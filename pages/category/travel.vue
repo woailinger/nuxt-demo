5b96e535c5f7a316ffd71f60c16fe1a81f5a5955
cokoro <you@example.com>
2020-08-23 17:38:17 +0800

    Upload all of the cagegory page

---
+++ pages/category/travel.vue
@@ -0,0 +1,13 @@
+<template>
+  <category-list type="Travel" />
+</template>
+
+<script>
+import CategoryList from './list'
+export default {
+  components: {CategoryList}
+}
+</script>
+
+<style scoped>
+</style>
