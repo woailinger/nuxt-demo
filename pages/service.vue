7e0432f55a6d21e278ca39d8b29715fd566110ae
huazheng <huazheng@lattebank.com>
2020-09-06 22:23:25 +0800

    service

--- pages/service.vue
+++ pages/service.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="service">
     <a-row>
+      <a-col :span="24">
+        <div style="height:60px"></div>
+      </a-col>
+      </a-row>
+    <a-row>
       <a-col :span="4" />
       <a-col :span="16">
-        <div><h1>Ashago Service (Coming soon)</h1></div>
+        <div style="height:60px">
+        <h1 style="color:#990000;font-size:40px;line-height:30px;font-family:Helvetica;">Ashago Service (coming soon)</h1>
+      </div>
       </a-col>
       <a-col :span="4" />
     </a-row>
@@ -18,13 +25,22 @@
     </a-row>
 
     <a-row>
-      <a-col :span="4" />
-      <a-col :span="16">
-        <p>Asha Go platform aims to address your needs and concerns about living in China. We hope to offer services to address your needs ranging from drivers, to cleaners and language instructors.</p>
-        <p>What services do you require that would make living in China easier for you? Please leave your ideas in the comment section below!</p>
+      <a-col :span="2" />
+      <a-col :span="20">
+        <div style="height:40px"></div>
+      <p style="color:#990000;font-size:20px;line-height:30px;font-family:Helvetica;">Asha Go platform aims to address your needs and concerns about living in China. </p>
+        <p style="color:#990000;font-size:20px;line-height:30px;font-family:Helvetica;">We hope to offer services to address your needs ranging from drivers, to cleaners and language instructors.</p>
+                <p style="color:#990000;font-size:20px;line-height:30px;font-family:Helvetica;">What services do you require that would make living in China easier for you?</p>
+                <p style="color:#990000;font-size:20px;line-height:30px;font-family:Helvetica;">Please drop us an email to let us better help you: info@ashago.com</p>
+
       </a-col>
-      <a-col :span="4" />
+      <a-col :span="2" />
     </a-row>
+    <a-row>
+      <a-col :span="24">
+        <div style="height:60px"></div>
+      </a-col>
+      </a-row>
   </div>
 </template>
 
@@ -34,7 +50,9 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #990000;
   margin-top: 10px;
+  line-height: 15px;
+  font-size: 15px;
 }
 </style>
