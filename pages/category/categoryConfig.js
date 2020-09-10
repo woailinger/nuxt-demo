d7e76ce3355df9b5c3efffbf90c234232480d823
cokoro <you@example.com>
2020-09-10 23:48:40 +0800

    Fix comment / food &drinks /padding

--- pages/category/categoryConfig.js
+++ pages/category/categoryConfig.js
@@ -1,7 +1,7 @@
 const config = {
     Food: {
       img: 'https://ashago-resource.oss-cn-zhangjiakou.aliyuncs.com/Asha%20Go%20Website%201.0/Category%20page/food%26drinks.jpg',
-      title: 'Food & Drink',
+      title: 'Food & Drinks',
       desc: 'Your guide to the best of China’s food and drink scene.',
       content: 'China has so much to offer when it comes to dining and drinking. In this section, we aim to provide you with a guide to all that China has to offer when it comes to food and drink! ',
     },
@@ -25,22 +25,7 @@ const config = {
       desc: 'The most useful Chinese language tips in your daily life.',
       content: 'Learning Chinese can be really challenging! We will provide you with useful Ianguage tips to help make your life easier.',
     
-    },
-    Service: {
-      img: 'https://ashago.oss-cn-zhangjiakou.aliyuncs.com/Asha%20Go%20China%20website%202020/Food%26Drinks/The%20most%20famous%20Chinese%20tea/%E9%A6%96%E9%A1%B5%E5%B0%81%E9%9D%A2%E5%9B%BE%E7%89%87.jpg?OSSAccessKeyId=LTAI4FcWHUa9TfvGA9oMY3fE&Expires=1001596708617&Signature=v0f%2BqrXbk%2BuRURvYNs8muA%2BeRuA%3D',
-      title: 'Service',
-      desc: 'Your guide to life in China.',
-      content: 'Living in China is like living on another planet! In this section, we provide you with articles, tips and strategies on how to live independently in China.',
-    
-    },
-    Community: {
-        img: 'https://ashago.oss-cn-zhangjiakou.aliyuncs.com/Asha%20Go%20China%20website%202020/Food%26Drinks/The%20most%20famous%20Chinese%20tea/%E9%A6%96%E9%A1%B5%E5%B0%81%E9%9D%A2%E5%9B%BE%E7%89%87.jpg?OSSAccessKeyId=LTAI4FcWHUa9TfvGA9oMY3fE&Expires=1001596708617&Signature=v0f%2BqrXbk%2BuRURvYNs8muA%2BeRuA%3D',
-        title: 'Community',
-        desc: 'Your guide to life in China.',
-        content: 'Living in China is like living on another planet! In this section, we provide you with articles, tips and strategies on how to live independently in China.',
-      
-      },
-    allTagList: ["Food", "Travel Tips"]
+    }
   }
   
   export default config
