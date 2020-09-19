e37a024a4f66097804e9e02b4bd4ccca2a8b4210
devdeng <devdeng@tencent.com>
2020-09-20 02:07:15 +0800

    up

--- plugins/config/product.js
+++ plugins/config/product.js
@@ -1,4 +1,4 @@
 let BaseConfig = {
-  host: 'http://ashago-api-dev.cc2dbe1fd91f042528f96dc27c2dba5fe.cn-zhangjiakou.alicontainer.com:80/' // 接口访问配置
+  host: 'http://ashago-api.cc2dbe1fd91f042528f96dc27c2dba5fe.cn-zhangjiakou.alicontainer.com:80/' // 接口访问配置
 }
 export default BaseConfig
